615ead1d0b1747c39133ee7ae155e98251a832d7
NicolasV <vanhuynegem.n@hotmail.com>
2019-04-27 07:31:20 +0200

    profile file start up

---
+++ profile.php
@@ -0,0 +1,23 @@
+<?php 
+    require_once("bootstrap.php");
+   
+    //$userName = $_SESSION['UserName'];
+    if( isset($_SESSION['User']) ){
+        //logged in user
+        echo "😎";
+    }else{
+        //no logged in user
+        header('Location: login.php');
+    }
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
